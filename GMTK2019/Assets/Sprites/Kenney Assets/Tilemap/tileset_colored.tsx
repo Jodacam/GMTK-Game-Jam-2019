5f0057cd1cfb5dca8c1f5a063008dbd4e5c7b68c
pabloski300 <pabloskidev@gmail.com>
2019-08-03 01:43:35 +0200

    Humito

--- GMTK2019/Assets/Sprites/Kenney Assets/Tilemap/tileset_colored.tsx
+++ GMTK2019/Assets/Sprites/Kenney Assets/Tilemap/tileset_colored.tsx
@@ -5,13 +5,19 @@
   <objectgroup draworder="index">
    <object id="2" x="0" y="0" width="16" height="16.0313"/>
   </objectgroup>
-  <animation>
-   <frame tileid="242" duration="100"/>
-   <frame tileid="243" duration="100"/>
-   <frame tileid="244" duration="100"/>
-   <frame tileid="245" duration="100"/>
-   <frame tileid="246" duration="100"/>
-   <frame tileid="247" duration="100"/>
-  </animation>
  </tile>
+ <wangsets>
+  <wangset name="Nuevo Conjunto Wang" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcornercolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="2" wangid="0x11111111"/>
+   <wangtile tileid="3" wangid="0x11111112"/>
+   <wangtile tileid="4" wangid="0x11111121"/>
+   <wangtile tileid="8" wangid="0x11111122"/>
+   <wangtile tileid="9" wangid="0x11111211"/>
+   <wangtile tileid="10" wangid="0x11111212"/>
+  </wangset>
+ </wangsets>
 </tileset>
