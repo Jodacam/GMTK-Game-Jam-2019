033e6198185f6f9ce2839407eac7ed31cf07f984
pabloski300 <pabloskidev@gmail.com>
2019-08-02 23:27:53 +0200

    cambios para que se vea bien pixel perfect

--- GMTK2019/Assets/Sprites/Kenney Assets/Tilemap/tileset_colored.tsx
+++ GMTK2019/Assets/Sprites/Kenney Assets/Tilemap/tileset_colored.tsx
@@ -1,4 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2018.12.22" name="colored" tilewidth="16" tileheight="16" spacing="1" tilecount="1024" columns="32">
+<tileset version="1.2" tiledversion="1.2.4" name="colored" tilewidth="16" tileheight="16" spacing="1" tilecount="1024" columns="32">
  <image source="../Tilesheet/colored.png" width="543" height="543"/>
+ <tile id="422">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16.0313"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="242" duration="100"/>
+   <frame tileid="243" duration="100"/>
+   <frame tileid="244" duration="100"/>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="247" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
