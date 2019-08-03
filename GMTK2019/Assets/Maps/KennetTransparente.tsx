ee542c7f104f6c02c9b3f00c25b7351551fd77fc
pabloski300 <pabloskidev@gmail.com>
2019-08-03 11:31:07 +0200

    Mapa de tienda efecto de pagar y coger monedas

--- GMTK2019/Assets/Maps/KennetTransparente.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="KennetTransparente" tilewidth="16" tileheight="16" spacing="1" tilecount="1024" columns="32">
- <image source="../Sprites/Kenney Assets/Tilesheet/colored_transparent.png" width="543" height="543"/>
-</tileset>
--- GMTK2019/Assets/Maps/Kenney.tsx
+++ GMTK2019/Assets/Maps/Kenney.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="colored" tilewidth="16" tileheight="16" spacing="1" tilecount="1056" columns="33">
- <image source="../Tilesheet/colored.png" width="560" height="543"/>
+ <image source="../Sprites/Kenney Assets/Tilesheet/colored.png" width="560" height="543"/>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="435">
   <objectgroup draworder="index">
    <object id="2" x="0" y="0" width="16" height="16.0313"/>
