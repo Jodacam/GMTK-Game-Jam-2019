a31ce2ed29072cb0d5fcf241134a50d522d8d96a
Jodacam <Jodacam2@gmail.com>
2019-08-04 18:57:14 +0200

    Corregido using vacio

--- GMTK2019/Assets/Scripts/GameController/GameController.cs
+++ GMTK2019/Assets/Scripts/GameController/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Unity.
+
 using UnityEngine.Rendering.PostProcessing;
 
 public class GameController : MonoBehaviour
